4e98a63b8902ef2792abb25e48e47deec3e712a3
bishop076 <271764729+bishop076@users.noreply.github.com>
2026-07-11 20:50:09 +0700

    feat: add /status slash command

---
+++ src/bot/commands/mod/status.ts
@@ -0,0 +1,34 @@
+import { executeStatus } from "@/core/handlers/command-handlers/mod/status-handler";
+import { safeDeferReply, safeEditReply } from "@/core/utils/command.utils";
+import { db } from "@/lib/db";
+import { memberCommandHistory } from "@/lib/db-schema";
+import { MessageFlags, type CommandInteraction } from "discord.js";
+import { Discord, Slash } from "discordx";
+
+@Discord()
+export class StatusCommand {
+  @Slash({
+    name: "status",
+    description: "Displays bot CPU and RAM usage in detail.",
+    dmPermission: false,
+  })
+  async status(interaction: CommandInteraction) {
+    if (!(await safeDeferReply(interaction, { flags: [MessageFlags.Ephemeral] })))
+      return;
+
+    if (interaction.member?.user.id && interaction.guildId) {
+      db.insert(memberCommandHistory)
+        .values({
+          channelId: interaction.channelId,
+          memberId: interaction.member.user.id,
+          guildId: interaction.guildId,
+          command: "status",
+        })
+        .catch(() => {});
+    }
+
+    const result = await executeStatus(interaction);
+
+    await safeEditReply(interaction, result);
+  }
+}
